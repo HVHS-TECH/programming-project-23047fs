c074d3bf5fa18520379121ef0c2f2d90f0e2a530
23047fs <23047fs@hvhs.school.nz>
2026-03-12 10:10:37 +1300

    formated code

--- game.js
+++ game.js
@@ -2,51 +2,51 @@
 // P5.play: Project Game
 /// Written by Finley
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
 
 //Constants
-    const canvaWidth = 500;
-    const canvaHeight = 500;
-    const numberOfEnemyBallsUp = 15;
-    const enemyBallDiameter = 20;
-    const minBallSpeed = 2;
-    const maxBallSpeed = 4;
-    const numberOfEnemyBeamsUp = 3;
-    const numberOfEnemiesInBeamsUp = 30;
-    const minBeamSpeed = 3;
-    const maxBeamSpeed = 5;
-    const numberOfScoreBallsLeft = 4;
-    const scoreBallDiameter = 10;
-    const playerJumpHeight = 9;
-    const playerSpeed = 4;
-    const timeOver = 30;
+const canvaWidth = 500;
+const canvaHeight = 500;
+const numberOfEnemyBallsUp = 15;
+const enemyBallDiameter = 20;
+const minBallSpeed = 2;
+const maxBallSpeed = 4;
+const numberOfEnemyBeamsUp = 3;
+const numberOfEnemiesInBeamsUp = 30;
+const minBeamSpeed = 3;
+const maxBeamSpeed = 5;
+const numberOfScoreBallsLeft = 4;
+const scoreBallDiameter = 10;
+const playerJumpHeight = 9;
+const playerSpeed = 4;
+const timeOver = 30;
 
 //Variables
-    //Screen phase: start = Start screen, game = game screen, lose = lose screen, win = win screen
-    let screenPhase = "start";
-    let screenPhaseSetup = false;
-    let scoreBallsCollected = 0;
-    let playerScore = 0;
-    let secondTimer = 0;
-    let timerSurvived = 0;
-    let enemyBeamX = 0;
+//Screen phase: start = Start screen, game = game screen, lose = lose screen, win = win screen
+let screenPhase = "start";
+let screenPhaseSetup = false;
+let scoreBallsCollected = 0;
+let playerScore = 0;
+let secondTimer = 0;
+let timerSurvived = 0;
+let enemyBeamX = 0;
 
 //Arrays
-    //Enemy ball sprites
-    let enemyBallSpriteArray = [];
-    //Enemy beam sprites
-    let enemyBeamSpriteArray = [];
-    //Score balls
-    let scoreBallSpriteArray = [];
+//Enemy ball sprites
+let enemyBallSpriteArray = [];
+//Enemy beam sprites
+let enemyBeamSpriteArray = [];
+//Score balls
+let scoreBallSpriteArray = [];
 
 //Timers
-    //Second timer
-    let intervalID = setInterval(() => {
-        secondTimer = secondTimer + 1;
-    }, 1000); //1000 ms timer 
+//Second timer
+let intervalID = setInterval(() => {
+    secondTimer = secondTimer + 1;
+}, 1000); //1000 ms timer 
 
 
 function preload() {
@@ -56,9 +56,9 @@ function preload() {
 }
 
 function setup() {
-	//Setup
-	console.log("Project Game");
-	cnv = new Canvas(canvaWidth, canvaHeight);
+    //Setup
+    console.log("Project Game");
+    cnv = new Canvas(canvaWidth, canvaHeight);
     //Center of canvas is 250, 250
     world.gravity.y = 20;
 
@@ -110,14 +110,14 @@ function setupPhases() {
 
         //Lose screen
         console.log("Lose Screen")
-        
+
     } else if (screenPhase == "win") {
         //Delete previous sprites
         deletePlayerSprite();
         deleteWallSprite();
         deleteEnemySprite();
         deleteScoreBallSprite();
-        
+
         //Set final score and time
         timerSurvived = secondTimer;
         playerScore = + scoreBallsCollected * secondTimer;
@@ -132,7 +132,7 @@ function setupPhases() {
 function startButtonSprite() {
     //Start Button
     startButton = new Sprite(250, 250, 80, 30, 'n');
-	startButton.color = '#3ceb07';
+    startButton.color = '#3ceb07';
 }
 
 function deleteStartButtonSprite() {
@@ -141,8 +141,8 @@ function deleteStartButtonSprite() {
 
 function playerSprite() {
     //Player
-	player = new Sprite(250, 250, 25, 25, 'd');
-	player.color = '#d31010';
+    player = new Sprite(250, 250, 25, 25, 'd');
+    player.color = '#d31010';
 }
 
 function deletePlayerSprite() {
@@ -151,25 +151,25 @@ function deletePlayerSprite() {
 
 function wallSprite() {
     //Walls
-	topWall = new Sprite(canvaWidth/2, 0, canvaHeight, 10, 'k');
-	topWall.color = '#a7f9ff';
+    topWall = new Sprite(canvaWidth / 2, 0, canvaHeight, 10, 'k');
+    topWall.color = '#a7f9ff';
     topWall.strokeWeight = 0;
     hitBoxGroup.add(topWall);
 
-    bottomWall = new Sprite(canvaWidth/2, canvaHeight, canvaHeight, 10, 'k');
-	bottomWall.color = '#a7f9ff';
+    bottomWall = new Sprite(canvaWidth / 2, canvaHeight, canvaHeight, 10, 'k');
+    bottomWall.color = '#a7f9ff';
     bottomWall.strokeWeight = 0;
     hitBoxGroup.add(bottomWall);
 
-    leftWall = new Sprite(0, canvaHeight/2, canvaHeight, 10, 'k');
+    leftWall = new Sprite(0, canvaHeight / 2, canvaHeight, 10, 'k');
     leftWall.rotation = 90;
-	leftWall.color = '#a7f9ff';
+    leftWall.color = '#a7f9ff';
     leftWall.strokeWeight = 0;
     hitBoxGroup.add(leftWall);
 
-    rightWall = new Sprite(canvaWidth, canvaHeight/2, canvaHeight, 10, 'k');
+    rightWall = new Sprite(canvaWidth, canvaHeight / 2, canvaHeight, 10, 'k');
     rightWall.rotation = 90;
-	rightWall.color = '#a7f9ff';
+    rightWall.color = '#a7f9ff';
     rightWall.strokeWeight = 0;
     hitBoxGroup.add(rightWall);
 }
@@ -183,7 +183,7 @@ function enemySprite() {
     //Enemy balls
     for (i = 0; i < numberOfEnemyBallsUp; i++) {
         enemyBallUp = new Sprite(random(25, 475), random(-40, 0), enemyBallDiameter, 'k');
-	    enemyBallUp.image = (imgSnowBall);
+        enemyBallUp.image = (imgSnowBall);
         imgSnowBall.resize(enemyBallDiameter, enemyBallDiameter);
         enemyBallUp.strokeWeight = 0;
         enemyBallUp.vel.y = random(minBallSpeed, maxBallSpeed);
@@ -230,13 +230,13 @@ function deleteScoreBallSprite() {
 // draw()
 /*******************************************************/
 function draw() {
-	background('#b8b8b8'); 
+    background('#b8b8b8');
 
     //Visual Variables
-	text("Mouse X " + round(mouse.x), 20, 20);
-	text("Mouse Y " + round(mouse.y), 20, 40);
-	text("Timer " + secondTimer, 20, 60);
-	text("Score " + scoreBallsCollected, 20, 80);
+    text("Mouse X " + round(mouse.x), 20, 20);
+    text("Mouse Y " + round(mouse.y), 20, 40);
+    text("Timer " + secondTimer, 20, 60);
+    text("Score " + scoreBallsCollected, 20, 80);
 
     //Screen Phases
     //What happens when the screen phases switch, what is deleted, what functions are drawn
@@ -275,7 +275,7 @@ function draw() {
         text("You have lost the game", 210, 200);
         text("You survived: " + timerSurvived + " seconds", 210, 220);
         text("You collected: " + scoreBallsCollected + " score balls", 210, 240);
-        text("You have scored: " + playerScore, 210, 260);    
+        text("You have scored: " + playerScore, 210, 260);
 
     } else if (screenPhase == "win") {
         if (screenPhaseSetup == true) {
@@ -288,14 +288,14 @@ function draw() {
         text("Wow you have won the game", 210, 200);
         text("You survived: " + timerSurvived + " seconds", 210, 220);
         text("You collected: " + scoreBallsCollected + " score balls", 210, 240);
-        text("You have scored: " + playerScore, 210, 260);  
+        text("You have scored: " + playerScore, 210, 260);
     };
 }
 
 //Switching screens and screen phases
 function startButtonFunction() {
     //Start button
-    if (mouseX > width/2 - startButton.w/2 && mouseX < width/2 + startButton.w/2 && mouseY > height/2 - startButton.h/2 && mouseY < height/2 + startButton.h/2 && mouseIsPressed) {
+    if (mouseX > width / 2 - startButton.w / 2 && mouseX < width / 2 + startButton.w / 2 && mouseY > height / 2 - startButton.h / 2 && mouseY < height / 2 + startButton.h / 2 && mouseIsPressed) {
         screenPhase = "game"
         screenPhaseSetup = true;
     };
@@ -304,25 +304,25 @@ function startButtonFunction() {
 //Movement and rotation with keyboard inputs
 function keyboardMovement() {
     //Resistance
-	player.vel.x = player.vel.x/1.04;
-	player.rotationSpeed = player.rotationSpeed/1.035;
+    player.vel.x = player.vel.x / 1.04;
+    player.rotationSpeed = player.rotationSpeed / 1.035;
 
-	//Left
+    //Left
     if (kb.pressing('a') || kb.pressing('arrowLeft')) {
-		// Set sprite's velocity to the left
-		player.vel.x = -1 * playerSpeed;
-	};
+        // Set sprite's velocity to the left
+        player.vel.x = -1 * playerSpeed;
+    };
 
-	//Right
-    if (kb.pressing ('d') || kb.pressing('arrowRight')) {
-		// Set sprite's velocity to the right
-		player.vel.x = playerSpeed;
-	};
+    //Right
+    if (kb.pressing('d') || kb.pressing('arrowRight')) {
+        // Set sprite's velocity to the right
+        player.vel.x = playerSpeed;
+    };
 
     //Jump
     if (kb.pressing('w') && hitBoxGroup.colliding(player) || kb.pressing('arrowUp') && hitBoxGroup.colliding(player)) {
         // Set sprite's velocity to the up
-		player.vel.y = -1 * playerJumpHeight;
+        player.vel.y = -1 * playerJumpHeight;
     }
 }
 
@@ -339,7 +339,7 @@ function enemyFunction() {
 
 function enemyStage1() {
     //Enemy balls only come from the left
-    if (secondTimer <= timeOver/3) {
+    if (secondTimer <= timeOver / 3) {
         //If the enemy goes off screen it respawns
         for (i = 0; i < enemyBallSpriteArray.length; i++) {
             if (enemyBallSpriteArray[i].y >= 500) {
@@ -349,7 +349,7 @@ function enemyStage1() {
                 console.log("Enemy ball reload");
             }
         }
-    } else if (secondTimer >= timeOver/3) {
+    } else if (secondTimer >= timeOver / 3) {
         //When time runs out
         for (i = 0; i < enemyBallSpriteArray.length; i++) {
             if (enemyBallSpriteArray[i].y >= 500) {
@@ -362,7 +362,7 @@ function enemyStage1() {
 
 function enemyStage2() {
     //Enemy beams come down from above
-    if (secondTimer >= (timeOver/3) + 2 && secondTimer <= (timeOver/3) * 2) {
+    if (secondTimer >= (timeOver / 3) + 2 && secondTimer <= (timeOver / 3) * 2) {
         //Beam enemies
         for (i = 0; i < enemyBeamSpriteArray.length; i++) {
             //Setting the speed
